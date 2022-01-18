a4e15f051a331ecb1c5566b8580921a456e8b733
baguskusumo <97827298+baguskusumo@users.noreply.github.com>
2022-01-18 05:46:55 -0800

    Delete koneksi.php

--- fungsi/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$host = "localhost";
-	$user = "root";
-	$password = "";
-	$db_name = "tugasbesar";
-	
-	$koneksi = mysqli_connect($host, $user, $password, $db_name) or die(mysqli_error());
-	$select_db = mysqli_select_db($koneksi, $db_name);
-?>
